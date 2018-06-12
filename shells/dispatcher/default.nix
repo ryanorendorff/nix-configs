a16e21dcc03af2612fe4e22acc06ef6a3f058993
Tom Doggett <nocoolnametom@gmail.com>
2018-06-11 17:58:56 -0700

    Updated shells with overlays and local configuration

--- shells/dispatcher/default.nix
+++ shells/dispatcher/default.nix
@@ -1,73 +1,90 @@
-with import <nixpkgs> { };
+let
+  # Set the variable "local_dir" to the project directory for the rest of the file.
+  local_dir = builtins.toString ./.;
+in with import <nixpkgs> {
+  # Overwrite the PHP packages to load config from $PROJECT_ROOT/.php/config - requires compilation
+  overlays = [
+    (self: super: let
+        applyLocalConfigDir = originalPackage: self.pkgs.lib.overrideDerivation originalPackage (old: rec {
+          configureFlags = originalPackage.configureFlags ++ [
+            "--with-config-file-scan-dir=${local_dir}/.php/config"
+          ];
+        });
+      in {
+        php56 = applyLocalConfigDir super.php56;
+        php70 = applyLocalConfigDir super.php70;
+        php71 = applyLocalConfigDir super.php71;
+        php72 = applyLocalConfigDir super.php72;
+    })
+  ];
+};
 
 let
-  php-apcu = pkgs.php71Packages.apcu;
-  php-ast = pkgs.php71Packages.ast;
-  php-composer = pkgs.php71Packages.composer;
-  php-igbinary = pkgs.php71Packages.igbinary;
-  php-phpcs = pkgs.php71Packages.phpcs;
-  php-redis = pkgs.php71Packages.redis;
-  php-xdebug = pkgs.php71Packages.xdebug;
-  php-yaml = pkgs.php71Packages.yaml;
-in pkgs.stdenv.mkDerivation rec {
+  packages = pkgs.php71Packages; # Alias the list of PHP packages to "packages"
+  php = pkgs.php71; # Alias the PHP package to "php"
+
+  # Ensure that composer is using the local environment's PHP executable
+  composer = packages.composer;
+
+  # Ensure that phpcs is using the local environment's PHP executable
+  phpcs = pkgs.lib.overrideDerivation packages.phpcs (old: rec {
+    installPhase = ''
+      mkdir -p $out/bin
+      install -D $src $out/libexec/phpcs/phpcs.phar
+      makeWrapper ${php}/bin/php $out/bin/phpcs \
+        --add-flags "$out/libexec/phpcs/phpcs.phar"
+    '';
+  });
+
+  # This is the config.ini for this project; it links the extensions required
+  configIniFile = pkgs.writeText "env_config.ini" ''
+    log_errors = 1
+    error_log = ${local_dir}/.php/php_errors.log
+    date.timezone = America/Los_Angeles
+    extension_dir = ${local_dir}/.php/extensions
+    extension = ${packages.apcu}/lib/php/extensions/apcu.so
+    extension = ${packages.ast}/lib/php/extensions/ast.so
+    extension = ${packages.igbinary}/lib/php/extensions/igbinary.so
+    extension = ${packages.redis}/lib/php/extensions/redis.so
+    extension = ${packages.yaml}/lib/php/extensions/yaml.so
+    zend_extension = ${packages.xdebug}/lib/php/extensions/xdebug.so
+    xdebug.remote_enable = 1
+    xdebug.remote_autostart = 1
+  '';
+in stdenv.mkDerivation rec {
+  # This name isn't really very important, but can help identify the project this derivation file
+  # belongs to.
   name = "zg-dispatcher-env";
 
-  # This is the list of packages used for this environment.
+  # This is the list of packages used for this environment. If it's here then it's available within
+  # the shell:
   buildInputs = with pkgs; [
-    fasd
-    makeWrapper
-    php-apcu
-    php-ast
-    php-composer
-    php-igbinary
-    php-phpcs
-    php-redis
-    php-xdebug
-    php-yaml
-    php71
+    less
+    packages.composer
+    packages.phpcs
+    php
     redis
   ];
 
-  # This sets up the environment within the shell, including a base PHP
-  # config file at `.php/extensions.ini`, places the composer `bin`
-  # directory within the path so you can run phpunit from the command line,
-  # and symlinks the installed binaries to `.php/bin` so tehy can be used
-  # in IDEs or however you may need them.
+  # This sets up the environment within the shell, places the composer `vendor/bin` directory within
+  # the path so you can run phpunit from the command line, and symlinks the installed binaries to
+  # `.php/bin` so they can be used in IDEs or however you may need them.
   shellHook = ''
     export PROJECT_HOME=`pwd`
-    export PHP_INI_SCAN_DIR=$PROJECT_HOME/.php
-    export PATH=$PROJECT_HOME/vendor/bin:$PATH
-    mkdir -p .php
-    if [ ! -s ".php/extensions.ini" ]; then
-    cat > .php/extensions.ini <<EOL
-      log_errors = 1
-      error_log = $PROJECT_HOME/.php/php_errors.log
-      date.timezone = America/Los_Angeles
-      extension_dir = $out/lib/php/extensions
-      extension = ${php-apcu}/lib/php/extensions/igbinary.so
-      extension = ${php-ast}/lib/php/extensions/ast.so
-      extension = ${php-igbinary}/lib/php/extensions/igbinary.so
-      extension = ${php-redis}/lib/php/extensions/redis.so
-      extension = ${php-yaml}/lib/php/extensions/yaml.so
-      extension = ${php-yaml}/lib/php/extensions/yaml.so
-      zend_extension = ${php-xdebug}/lib/php/extensions/xdebug.so
-      xdebug.remote_enable = 1
-      xdebug.remote_autostart = 1
-    EOL
-    fi
-    if [ -L ".php/bin" ]; then
-      rm .php/bin
-    fi
-    ln -s $env/bin .php/bin
+    export PATH=$PROJECT_HOME/.php/bin:$PROJECT_HOME/vendor/bin:$PATH
+    mkdir -p $PROJECT_HOME/.php/{extensions,config}
+    rm -f $PROJECT_HOME/.php/bin && ln -s $env/bin $PROJECT_HOME/.php/bin
+    rm -f $PROJECT_HOME/.php/config/env_config.ini && ln -s ${configIniFile} $PROJECT_HOME/.php/config/env_config.ini
   '';
 
+  # This contains instructions to wrap the 
   env = buildEnv {
     name = name;
     paths = buildInputs;
-    buildInputs = [ makeWrapper ];
+    buildInputs = [ pkgs.makeWrapper ];
     postBuild = ''
-      mkdir $out/bin.writable && cp --symbolic-link `readlink $out/bin`/* $out/bin.writable/ && rm $out/bin && mv $out/bin.writable $out/bin
-      wrapProgram $out/bin/php --add-flags ""
+      mkdir $out/bin.writable && cp --symbolic-link `readlink $out/bin`/* $out/bin.writable/ > /dev/null 2>&1 && rm $out/bin && mv $out/bin.writable $out/bin
+      wrapProgram $out/bin/redis-server --add-flags "--port 10010"
     '';
   };
 
--- shells/php55/default.nix
+++ shells/php55/default.nix
@@ -1,127 +1,169 @@
-with import <nixpkgs> { };
+let
+  # Set the variable "local_dir" to the project directory for the rest of the file.
+  local_dir = builtins.toString ./.;
+in with import <nixpkgs> {
+  # Overwrite the PHP packages to load config from $PROJECT_ROOT/.php/config - requires compilation
+  overlays = [
+    (self: super: let
+        applyLocalConfigDir = originalPackage: self.pkgs.lib.overrideDerivation originalPackage (old: rec {
+          configureFlags = originalPackage.configureFlags ++ [
+            "--with-config-file-scan-dir=${local_dir}/.php/config"
+          ];
+        }
+      );
+      in {
+        php55 = self.lib.overrideDerivation super.php56 (old: rec {
+          version = "5.5.37";
+          name = "php-${version}";
+          src = self.fetchurl {
+            url = "https://secure.php.net/distributions/php-${version}.tar.bz2";
+            sha256 = "d2380ebe46caf17f2c4cd055867d00a82e6702dc5f62dc29ce864a5742905d88";
+          };
+          configureFlags = super.php56.configureFlags ++ [
+            "--with-config-file-scan-dir=${local_dir}/.php/config"
+          ];
+        });
+        php55Packages = {
+          composer = self.stdenv.mkDerivation rec {
+            name = "composer-${version}";
+            version = "1.6.3";
+            src = self.fetchurl {
+              url = "https://getcomposer.org/download/${version}/composer.phar";
+              sha256 = "1dna9ng77nw002l7hq60b6vz0f1snmnsxj1l7cg4f877msxppjsj";
+            };
+            unpackPhase = ":";
+            buildInputs = [ self.makeWrapper ];
+            installPhase = ''
+              mkdir -p $out/bin
+              install -D $src $out/libexec/composer/composer.phar
+              makeWrapper ${self.php55}/bin/php $out/bin/composer \
+                --add-flags "$out/libexec/composer/composer.phar"
+            '';
+          };
+          phpcs = self.stdenv.mkDerivation rec {
+            name = "phpcs-${version}";
+            version = "3.3.0";
+            src = self.fetchurl {
+              url = "https://github.com/squizlabs/PHP_CodeSniffer/releases/download/${version}/phpcs.phar";
+              sha256 = "1zl35vcq8dmspsj7ww338h30ah75dg91j6a1dy8avkzw5zljqi4h";
+            };
+            phases = [ "installPhase" ];
+            buildInputs = [ self.makeWrapper ];
+            installPhase = ''
+              mkdir -p $out/bin
+              install -D $src $out/libexec/phpcs/phpcs.phar
+              makeWrapper ${self.php55}/bin/php $out/bin/phpcs \
+                --add-flags "$out/libexec/phpcs/phpcs.phar"
+            '';
+          };
+          xdebug = self.stdenv.mkDerivation rec {
+            name = "xdebug-2.3.1";
+            src = self.fetchurl {
+              url = "http://pecl.php.net/get/${name}.tgz";
+              sha256 = "0k567i6w7cw14m13s7ip0946pvy5ii16cjwjcinnviw9c24na0xm";
+            };
+            autoreconfPhase = "phpize";
+            preConfigure = "touch unix.h";
+            doCheck = true;
+            checkTarget = "test";
+            buildInputs = with self; [ autoreconfHook php55 ];
+            makeFlags = [ "EXTENSION_DIR=$(out)/lib/php/extensions" ];
+          };
+          xcache = self.stdenv.mkDerivation rec {
+            name = "xcache-${version}";
+            version = "3.2.0";
+            src = self.fetchurl {
+              url = "http://xcache.lighttpd.net/pub/Releases/${version}/${name}.tar.bz2";
+              sha256 = "1gbcpw64da9ynjxv70jybwf9y88idm01kb16j87vfagpsp5s64kx";
+            };
+            autoreconfPhase = "phpize";
+            preConfigure = "touch unix.h";
+            doCheck = true;
+            checkTarget = "test";
+            configureFlags = [
+              "--enable-xcache"
+              "--enable-xcache-coverager"
+              "--enable-xcache-optimizer"
+              "--enable-xcache-assembler"
+              "--enable-xcache-encoder"
+              "--enable-xcache-decoder"
+            ];
+            buildInputs = with self; [ autoreconfHook php55 m4 ];
+            makeFlags = [ "EXTENSION_DIR=$(out)/lib/php/extensions" ];
+          };
+          yaml = self.stdenv.mkDerivation rec {
+            name = "yaml-1.3.1";
+            src = self.fetchurl {
+              url = "http://pecl.php.net/get/${name}.tgz";
+              sha256 = "1fbmgsgnd6l0d4vbjaca0x9mrfgl99yix5yf0q0pfcqzfdg4bj8q";
+            };
+            configureFlags = [
+              "--with-yaml=${self.libyaml}"
+            ];
+            autoreconfPhase = "phpize";
+            preConfigure = "touch unix.h";
+            doCheck = true;
+            checkTarget = "test";
+            buildInputs = with self; [ autoreconfHook php55 libyaml ];
+            makeFlags = [ "EXTENSION_DIR=$(out)/lib/php/extensions" ];
+          };
+        };
+        php56 = applyLocalConfigDir super.php56;
+        php70 = applyLocalConfigDir super.php70;
+        php71 = applyLocalConfigDir super.php71;
+        php72 = applyLocalConfigDir super.php72;
+    })
+  ];
+};
 
 let
-  # These are packages set up to explicitly match what is available on Stage or Prod
+  packages = pkgs.php55Packages; # Alias the list of PHP packages to "packages"
+  php = pkgs.php55; # Alias the PHP package to "php"
 
-  # PHP v5.5.37
-  php55 = pkgs.lib.overrideDerivation pkgs.php56 (old: rec {
-    version = "5.5.37";
-    name = "php-${version}";
-    src = fetchurl {
-      url = "https://secure.php.net/distributions/php-${version}.tar.bz2";
-      sha256 = "d2380ebe46caf17f2c4cd055867d00a82e6702dc5f62dc29ce864a5742905d88";
-    };
-    configureFlags = pkgs.php56.configureFlags ++ [
-      "--with-config-file-scan-dir=/etc/php.d/php55"
-    ];
-  });
-
-  # XDebug for PHP 5.5.37
-  php55_xdebug = pkgs.stdenv.mkDerivation rec {
-    name = "xdebug-2.3.1";
-    src = fetchurl {
-      url = "http://pecl.php.net/get/${name}.tgz";
-      sha256 = "0k567i6w7cw14m13s7ip0946pvy5ii16cjwjcinnviw9c24na0xm";
-    };
-    autoreconfPhase = "phpize";
-    preConfigure = "touch unix.h";
-    doCheck = true;
-    checkTarget = "test";
-    buildInputs = with pkgs; [ autoreconfHook php55 ];
-    makeFlags = [ "EXTENSION_DIR=$(out)/lib/php/extensions" ];
-  };
-
-  # XCache for PHP 5.5.37 (this allows for symfony in-memory caching to occur)
-  php55_xcache = pkgs.stdenv.mkDerivation rec {
-    name = "xcache-${version}";
-    version = "3.2.0";
-    src = fetchurl {
-      url = "http://xcache.lighttpd.net/pub/Releases/${version}/${name}.tar.bz2";
-      sha256 = "1gbcpw64da9ynjxv70jybwf9y88idm01kb16j87vfagpsp5s64kx";
-    };
-    autoreconfPhase = "phpize";
-    preConfigure = "touch unix.h";
-    doCheck = true;
-    checkTarget = "test";
-    configureFlags = [
-      "--enable-xcache"
-      "--enable-xcache-coverager"
-      "--enable-xcache-optimizer"
-      "--enable-xcache-assembler"
-      "--enable-xcache-encoder"
-      "--enable-xcache-decoder"
-    ];
-    buildInputs = with pkgs; [ autoreconfHook php55 m4 ];
-    makeFlags = [ "EXTENSION_DIR=$(out)/lib/php/extensions" ];
-  };
-
-  # YAML for PHP 5.5.37
-  php55_yaml = pkgs.stdenv.mkDerivation rec {
-    name = "yaml-1.3.1";
-    src = fetchurl {
-      url = "http://pecl.php.net/get/${name}.tgz";
-      sha256 = "1fbmgsgnd6l0d4vbjaca0x9mrfgl99yix5yf0q0pfcqzfdg4bj8q";
-    };
-    configureFlags = [
-      "--with-yaml=${pkgs.libyaml}"
-    ];
-    autoreconfPhase = "phpize";
-    preConfigure = "touch unix.h";
-    doCheck = true;
-    checkTarget = "test";
-    buildInputs = with pkgs; [ autoreconfHook php55 libyaml ];
-    makeFlags = [ "EXTENSION_DIR=$(out)/lib/php/extensions" ];
-  };
-
-in
-
-pkgs.stdenv.mkDerivation rec {
+  # This is the config.ini for this project; it links the extensions required
+  configIniFile = pkgs.writeText "env_config.ini" ''
+    log_errors = 1
+    error_log = ${local_dir}/.php/php_errors.log
+    date.timezone = America/Los_Angeles
+    extension_dir = ${local_dir}/.php/extensions
+    extension = ${packages.xcache}/lib/php/extensions/xcache.so
+    extension = ${packages.yaml}/lib/php/extensions/yaml.so
+    zend_extension = ${packages.xdebug}/lib/php/extensions/xdebug.so
+    xdebug.remote_enable = 1
+    xdebug.remote_autostart = 1
+  '';
+in stdenv.mkDerivation rec {
+  # This name isn't really very important, but can help identify the project this derivation file
+  # belongs to.
   name = "zg-php55-env";
 
-  # This is the list of packages used for this environment.
+  # This is the list of packages used for this environment. If it's here then it's available within
+  # the shell:
   buildInputs = with pkgs; [
-    php55
-    php55_xdebug
-    php55_yaml
-    php55_xcache
-    makeWrapper
+    less
+    packages.composer
+    packages.phpcs
+    php
   ];
 
-  # This sets up the environment within the shell, including a base PHP
-  # config file at `.php/extensions.ini`, places the composer `bin`
-  # directory within the path so you can run phpunit from the command line,
-  # and symlinks the installed binaries to `.php/bin` so tehy can be used
-  # in IDEs or however you may need them.
+  # This sets up the environment within the shell, places the composer `vendor/bin` directory within
+  # the path so you can run phpunit from the command line, and symlinks the installed binaries to
+  # `.php/bin` so they can be used in IDEs or however you may need them.
   shellHook = ''
     export PROJECT_HOME=`pwd`
-    export PHP_INI_SCAN_DIR=$PROJECT_HOME/.php
-    export PATH=$PROJECT_HOME/bin:$PATH
-    mkdir -p .php
-    if [ ! -s ".php/extensions.ini" ]; then
-    cat > .php/extensions.ini <<EOL
-      log_errors = 1
-      error_log = $PROJECT_HOME/.php/php_errors.log
-      date.timezone = America/Los_Angeles
-      extension_dir = $out/lib/php/extensions
-      zend_extension = ${php55_xdebug}/lib/php/extensions/xdebug.so
-      xdebug.remote_enable = 1
-      xdebug.remote_autostart = 1
-      extension = ${php55_yaml}/lib/php/extensions/yaml.so
-      extension = ${php55_xcache}/lib/php/extensions/xcache.so
-    EOL
-    fi
-    if [ -L ".php/bin" ]; then
-      rm .php/bin
-    fi
-    ln -s $env/bin .php/bin
+    export PATH=$PROJECT_HOME/.php/bin:$PROJECT_HOME/vendor/bin:$PATH
+    mkdir -p $PROJECT_HOME/.php/{extensions,config}
+    rm -f $PROJECT_HOME/.php/bin && ln -s $env/bin $PROJECT_HOME/.php/bin
+    rm -f $PROJECT_HOME/.php/config/env_config.ini && ln -s ${configIniFile} $PROJECT_HOME/.php/config/env_config.ini
   '';
 
+  # This contains instructions to wrap the 
   env = buildEnv {
     name = name;
     paths = buildInputs;
-    buildInputs = [ makeWrapper ];
+    buildInputs = [ pkgs.makeWrapper ];
     postBuild = ''
-      mkdir $out/bin.writable && cp --symbolic-link `readlink $out/bin`/* $out/bin.writable/ && rm $out/bin && mv $out/bin.writable $out/bin
+      mkdir $out/bin.writable && cp --symbolic-link `readlink $out/bin`/* $out/bin.writable/ > /dev/null 2>&1 && rm $out/bin && mv $out/bin.writable $out/bin
       wrapProgram $out/bin/php --add-flags ""
     '';
   };
