dcbaa786d96e41ef9594fe62d05444e3d8423a29
Tom Doggett <nocoolnametom@gmail.com>
2018-10-13 11:57:15 -0700

    Fixing personal repos script

--- pkgs/scripts/personal_startup/default.nix
+++ pkgs/scripts/personal_startup/default.nix
@@ -1,32 +1,32 @@
 { pkgs, lib, ... }:
 
 let
-  projectsPath = if pkgs.stdenv.isDarwin then "~/Projects/" else "~/projects/";
+  projectsPath = if pkgs.stdenv.isDarwin then "~/Projects" else "~/projects";
   downloadsVBoxPath = "/mnt/vbox/sdcard/Downloads";
   downloadsVmwarePath = "/mnt/vmware/downloads";
 in pkgs.writeScript "personal_startup" (
   ''
     #!/usr/bin/env bash
-    mkdir -p ${projectsPath}{nocoolnametom}
+    mkdir -p "${projectsPath}/nocoolnametom"
 
-    # if [ ! -d ${projectsPath}nocoolnametom/home-assistant-config ]; then
-    #   git clone git@github.com:nocoolnametom/home-assistant-config.git ${projectsPath}nocoolnametom/home-assistant-config ;
+    # if [ ! -d "${projectsPath}/nocoolnametom/home-assistant-config" ]; then
+    #   git clone git@github.com:nocoolnametom/home-assistant-config.git "${projectsPath}/nocoolnametom/home-assistant-config" ;
     # fi;
 
-    if [ ! -d ${projectsPath}nocoolnametom/cesletterbox ]; then
-      git clone git@github.com:nocoolnametom/cesletterbox.git ${projectsPath}nocoolnametom/cesletterbox ;
+    if [ ! -d "${projectsPath}/nocoolnametom/cesletterbox" ]; then
+      git clone git@github.com:nocoolnametom/cesletterbox.git "${projectsPath}/nocoolnametom/cesletterbox" ;
     fi;
 
-    # if [ ! -d ${projectsPath}nocoolnametom/my-blog ]; then
-    #   git clone git@github.com:nocoolnametom/my-blog.git ${projectsPath}nocoolnametom/my-blog ;
+    # if [ ! -d "${projectsPath}/nocoolnametom/my-blog" ]; then
+    #   git clone git@github.com:nocoolnametom/my-blog.git "${projectsPath}/nocoolnametom/my-blog" ;
     # fi;
 
-    if [ ! -d ${projectsPath}nocoolnametom/nix-configs ]; then
-      git clone git@github.com:nocoolnametom/nix-configs.git ${projectsPath}nocoolnametom/nix-configs ;
+    if [ ! -d "${projectsPath}/nocoolnametom/nix-configs" ]; then
+      git clone git@github.com:nocoolnametom/nix-configs.git "${projectsPath}/nocoolnametom/nix-configs" ;
     fi;
 
-    if [ ! -d ${projectsPath}nocoolnametom/wiki_copy ]; then
-      git clone git@github.com:nocoolnametom/wiki_copy.git ${projectsPath}nocoolnametom/wiki_copy ;
+    if [ ! -d "${projectsPath}/nocoolnametom/wiki_copy" ]; then
+      git clone git@github.com:nocoolnametom/wiki_copy.git "${projectsPath}/nocoolnametom/wiki_copy" ;
     fi;
   '' + (
     lib.optionalString (!pkgs.stdenv.isDarwin) ''
