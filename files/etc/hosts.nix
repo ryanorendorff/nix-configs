8bde79a5a2261137bee6eab1aa3d0ea4ea192f66
Tom Doggett <nocoolnametom@gmail.com>
2018-11-28 13:37:51 -0800

    Fixing work DNS issues around chunkwm

--- files/etc/hosts.nix
+++ files/etc/hosts.nix
@@ -5,10 +5,10 @@
 {
   "${prefix}hosts" = {
     text = ''
-      127.0.0.1       localhost ${hostName}
+      127.0.0.1       localhost
       255.255.255.255 broadcasthost
-      ::1             localhost ${hostName}
-      fe80::1%lo0	    localhost ${hostName}
+      ::1             localhost
+      fe80::1%lo0	    localhost
 
       ${extraHosts}
     '';
